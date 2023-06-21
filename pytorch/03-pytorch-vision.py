2994929e3d62eaf183f5293874e30ad027072ab6
jliu <liujun@ktjr.com>
2023-06-21 19:24:55 +0800

    vision

--- pytorch/03-pytorch-vision.py
+++ pytorch/03-pytorch-vision.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+from timeit import default_timer as timer
 import torch
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from helper_functions import *
+from tqdm.auto import tqdm
 
 train_data = datasets.FashionMNIST(
     root="data",
@@ -20,11 +22,11 @@ test_data = datasets.FashionMNIST(
 )
 class_names = train_data.classes
 
-imgae, label = train_data[0]
-print(imgae.shape)
-plt.imshow(imgae.squeeze())
-plt.title(label)
-plt.show()
+# imgae, label = train_data[0]
+# print(imgae.shape)
+# plt.imshow(imgae.squeeze())
+# plt.title(label)
+# plt.show()
 
 BATCH_SIZE = 32
 train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
@@ -39,13 +41,10 @@ class FashionMNISTModelV0(torch.nn.Module):
             torch.nn.Linear(input_shape, hidden_units),
             torch.nn.Linear(hidden_units, output_shape)
         )
-     
-
     def forward(self, x):
         return self.layer_stack(x)
 
 
-torch.manual_seed(42)
 model_0 = FashionMNISTModelV0(
     input_shape=28*28,
     hidden_units=10,
@@ -55,3 +54,41 @@ model_0.to('cpu')
 
 loss_fn = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model_0.parameters(), lr=0.1)
+
+torch.manual_seed(42)
+train_time_start_on_cpu = timer()
+
+epochs = 3
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}")
+    train_loss = 0
+    for batch, (X, y) in enumerate(train_dataloader):
+        model_0.train()
+        y_pred = model_0(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 400 == 0:
+            print(f"look at {batch * len(X)}/{len(train_dataloader.dataset)}")
+
+    train_loss /= len(train_dataloader)
+
+    test_loss, test_acc = 0, 0
+    model_0.eval()
+    with torch.inference_mode():
+        for X, y in test_dataloader:
+            test_pred = model_0(X)
+            test_loss += loss_fn(test_pred, y)
+            test_acc += accuracy_fn(y, test_pred.argmax(dim=1))
+
+        test_loss /= len(test_dataloader)
+        test_acc /= len(test_dataloader)
+    print(f"Train loss: {train_loss}, Test loss: {test_loss}, Test acc: {test_acc}")
+
+train_time_end_on_cpu = timer()
+total_train_time_model_0 = print_train_time(train_time_start_on_cpu, train_time_end_on_cpu)
+device = str(next(model_0.parameters()).device)
